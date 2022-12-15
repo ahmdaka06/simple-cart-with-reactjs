0e79bae3aad48eb7541d9804e2793c223f0ca8be
Ahmad Andika <ahmdaka06@gmail.com>
2022-12-15 19:14:42 +0700

    push

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -15,35 +15,37 @@ const Product  = ({addToCart}) => {
         setProducts(response);
     }
     return (
-        <div class="overflow-x-auto relative"><div className="card-custom">
-        <div className="card-body">
-          <h2 className="card-title">Produk</h2>
-          <hr />
-          <div class="overflow-x-auto relative">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead>
-                <tr>
-                  <th>Nama Produk</th>
-                  <th className='text-center'>Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-              {
-                products.map((product) => (
-                <tr key={product.id}>
-                  <td>{ product.name }</td>
-                  <td className='text-center'>
-                    <button className="btn btn-custom-primary" onClick={() => addToCart(product)}><md.MdAddShoppingCart /></button>
-                  </td>
-                </tr>
-                ))
-              }
-                
-              </tbody>
-            </table>
+        <div class="overflow-x-auto relative">
+          <div className="card-custom">
+            <div className="card-body">
+              <h2 className="card-title">Produk</h2>
+              <hr />
+              <div class="overflow-x-auto relative">
+              <table className="table w-full  dark:text-slate-900">
+                  <thead>
+                    <tr>
+                      <th>Nama Produk</th>
+                      <th className='text-center'>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {
+                    products.map((product) => (
+                    <tr key={product.id}>
+                      <td>{ product.name }</td>
+                      <td className='text-center'>
+                        <button className="btn btn-sm btn-success" onClick={() => addToCart(product)}><md.MdAddShoppingCart /></button>
+                      </td>
+                    </tr>
+                    ))
+                  }
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
-      </div></div>
         
     )
 }
